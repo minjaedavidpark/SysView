d904526e954df08784b0c3e600277bdb8931c63b
Min Jae David Park <mjdavid.park@mail.utoronto.ca>
2024-04-02 17:23:56 -0400

    Set up command-line interface

--- main.cpp
+++ main.cpp
@@ -6,32 +6,88 @@
 #include <cctype>
 #include <cstdio>
 
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 // Reference: https://viewsourcecode.org/snaptoken/kilo/02.enteringRawMode.html
 struct termios orig_termios;
 
+struct editorConfig {
+    struct termios orig_termios;
+};
+struct editorConfig E;
+
+void die(const char *s) {
+    // Clear the screen upon exit
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+    perror(s);
+    exit(1);
+}
+
 // Disable raw mode
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
+        die("tcsetattr");
 }
 
 // Enable raw mode
 void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
     raw.c_lflag &= ~(ECHO | ICANON);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
+
+char editorReadKey() {
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) die("read");
+    }
+    return c;
+}
+
+/*** input ***/
+void editorProcessKeypress() {
+    char c = editorReadKey();
+    switch (c) {
+        case ('q'):
+            // Clear the screen upon exit
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+            exit(0);
+            break;
+    }
+}
+
+void editorDrawRows() {
+    int y;
+    for (y = 0; y < 24; y++) {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
+void editorRefreshScreen() {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+
+
+}
+
+
 
 int main() {
     enableRawMode();
     // User input
-    char c;
-    // Terminate the program if the user enters 'q'
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
-
+    while (1) {
+        editorRefreshScreen();
+        std::cout << Color::Modifier(Color::FG_BLUE) << "Press Q to exit" << Color::Modifier(Color::FG_DEFAULT) << std::endl;
+        editorProcessKeypress();
     }
 
-    disableRawMode();
     return 0;
 }
